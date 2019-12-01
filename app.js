b96e70d417cb96c5cc2004a312a37bb1e672d663
fed <korolevfedos@yandex.ru>
2019-12-02 11:52:52 +1200

    /

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 var cors = require("cors"); //дополнительные заголовки отдаваемые сервером(обход блокировок браузера)
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
 var transferDataRouter = require("./routes/transferData");
 
 var app = express();
@@ -33,13 +31,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-//
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testAPI', testAPIRouter);
 app.use('/transferData', transferDataRouter);
-
 
 
 // catch 404 and forward to error handler
@@ -47,12 +40,12 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
